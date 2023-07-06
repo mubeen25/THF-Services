6112e46c6d8ddd17278d39431b2d410ea6c518b1
mubeen25 <mobinsoomro21@gmail.com>
2023-07-06 22:14:15 +0500

    Router update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 import './App.css'
 import Navigation from './Components/Navigation'
 import Title from './Components/Title'
-import Contact from './Components/Contact'
 import Services from './Components/Services'
-<<<<<<< HEAD
+
 import About from './Components/About';
 import Contact from './Components/Contact';
 import {Routes, Route } from 'react-router-dom'
-import About from './Components/About'
->>>>>>> b9d3847c1e268dffbc73f049b9f0ec68e089ac17
+
 
 function App() {
   return (
